e54d26842415e1f142a9206666d62adfec7a853e
Inkounter <inkounter@gmail.com>
2026-02-01 07:14:58 -0500

    Move settings into its own file.

---
+++ settings.lua
@@ -0,0 +1,184 @@
+local thisAddonName, namespace = ...
+
+namespace.Settings = {
+    ["defaultConfig"] = {
+        ["anchorFrame"] = "UIParent",
+        ["anchorPoint"] = "TOP",
+        ["anchorRelativeTo"] = "TOP",
+        ["xOffset"] = 0,
+        ["yOffset"] = 0,
+    },
+
+    -- Overwrite the `KeystoneTextConfig` global variable with a (shallow) copy
+    -- of the defaults.  Note that this does not apply any changes to the UI.
+    ["assignDefaultConfig"] = function(self)
+        KeystoneTextConfig = {}
+        local config = KeystoneTextConfig
+        for k, v in pairs(self.defaultConfig) do
+            config[k] = v
+        end
+    end,
+
+    -- Re-anchor the text frame.
+    ["reanchor"] = function()
+        local config = KeystoneTextConfig
+        local frame = namespace.frame
+        local parent = _G[config.anchorFrame]
+        if parent then
+            frame:ClearAllPoints()
+            frame:SetPoint(
+                config.anchorPoint,
+                parent,
+                config.anchorRelativeTo,
+                config.xOffset,
+                config.yOffset
+            )
+            frame:Show()
+        else
+            print(
+                "Failed to anchor keystone text to nonexistent frame: "
+                .. config.anchorFrame
+            )
+            frame:Hide()
+        end
+    end,
+
+    -- Register the settings with the UI.
+    ["register"] = function(self)
+        local ac = LibStub("AceConfig-3.0")
+        local optionsTable = {
+            ["name"] = thisAddonName,
+            ["type"] = "group",
+            ["args"] = {
+                ["anchorFrame"] = {
+                    ["order"] = 0,
+                    ["name"] = "Anchor To Frame",
+                    ["type"] = "input",
+                    ["set"] = function(info, value)
+                        KeystoneTextConfig.anchorFrame = value
+                        self:reanchor()
+                    end,
+                    ["get"] = function(info)
+                        return KeystoneTextConfig.anchorFrame
+                    end,
+                },
+
+                ["anchorPoint"] = {
+                    ["order"] = 1,
+                    ["name"] = "Anchor Point",
+                    ["type"] = "select",
+                    ["style"] = "dropdown",
+                    ["values"] = {
+                        ["TOPLEFT"]     = "TOPLEFT",
+                        ["TOP"]         = "TOP",
+                        ["TOPRIGHT"]    = "TOPRIGHT",
+                        ["LEFT"]        = "LEFT",
+                        ["CENTER"]      = "CENTER",
+                        ["RIGHT"]       = "RIGHT",
+                        ["BOTTOMLEFT"]  = "BOTTOMLEFT",
+                        ["BOTTOM"]      = "BOTTOM",
+                        ["BOTTOMRIGHT"] = "BOTTOMRIGHT",
+                    },
+                    ["sorting"] = {
+                        "TOPLEFT",
+                        "TOP",
+                        "TOPRIGHT",
+                        "LEFT",
+                        "CENTER",
+                        "RIGHT",
+                        "BOTTOMLEFT",
+                        "BOTTOM",
+                        "BOTTOMRIGHT",
+                    },
+                    ["set"] = function(info, value)
+                        KeystoneTextConfig.anchorPoint = value
+                        self:reanchor()
+                    end,
+                    ["get"] = function(info)
+                        return KeystoneTextConfig.anchorPoint
+                    end,
+                },
+
+                ["anchorRelativeTo"] = {
+                    ["order"] = 2,
+                    ["name"] = "Anchor Relative To",
+                    ["type"] = "select",
+                    ["style"] = "dropdown",
+                    ["values"] = {
+                        ["TOPLEFT"]     = "TOPLEFT",
+                        ["TOP"]         = "TOP",
+                        ["TOPRIGHT"]    = "TOPRIGHT",
+                        ["LEFT"]        = "LEFT",
+                        ["CENTER"]      = "CENTER",
+                        ["RIGHT"]       = "RIGHT",
+                        ["BOTTOMLEFT"]  = "BOTTOMLEFT",
+                        ["BOTTOM"]      = "BOTTOM",
+                        ["BOTTOMRIGHT"] = "BOTTOMRIGHT",
+                    },
+                    ["sorting"] = {
+                        "TOPLEFT",
+                        "TOP",
+                        "TOPRIGHT",
+                        "LEFT",
+                        "CENTER",
+                        "RIGHT",
+                        "BOTTOMLEFT",
+                        "BOTTOM",
+                        "BOTTOMRIGHT",
+                    },
+                    ["set"] = function(info, value)
+                        KeystoneTextConfig.anchorRelativeTo = value
+                        self:reanchor()
+                    end,
+                    ["get"] = function(info)
+                        return KeystoneTextConfig.anchorRelativeTo
+                    end,
+                },
+
+                ["xOffset"] = {
+                    ["order"] = 3,
+                    ["name"] = "X-Offset",
+                    ["type"] = "range",
+                    ["softMin"] = -500,
+                    ["softMax"] = 500,
+                    ["set"] = function(info, value)
+                        KeystoneTextConfig.xOffset = value
+                        self:reanchor()
+                    end,
+                    ["get"] = function(info)
+                        return KeystoneTextConfig.xOffset
+                    end,
+                },
+
+                ["yOffset"] = {
+                    ["order"] = 4,
+                    ["name"] = "Y-Offset",
+                    ["type"] = "range",
+                    ["softMin"] = -500,
+                    ["softMax"] = 500,
+                    ["set"] = function(info, value)
+                        KeystoneTextConfig.yOffset = value
+                        self:reanchor()
+                    end,
+                    ["get"] = function(info)
+                        return KeystoneTextConfig.yOffset
+                    end,
+                },
+
+                ["resetDefault"] = {
+                    ["type"] = "execute",
+                    ["order"] = 5,
+                    ["name"] = "Reset to Default",
+                    ["func"] = function()
+                        self:assignDefaultConfig()
+                        self:reanchor()
+                    end,
+                }
+            },
+        }
+        ac:RegisterOptionsTable(thisAddonName, optionsTable, nil)
+
+        local acd = LibStub("AceConfigDialog-3.0")
+        return acd:AddToBlizOptions(thisAddonName)
+    end,
+}
